094fa1237ef99b8c6fb2bba2f1c2af051352cd5d
HummerBin <zhang805010@163.com>
2018-09-04 09:48:27 +0800

    api 网关加入到服务注册中心

--- api-gateway/src/main/java/com/zhph/security/gateway/Application.java
+++ api-gateway/src/main/java/com/zhph/security/gateway/Application.java
@@ -2,6 +2,7 @@ package com.zhph.security.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
